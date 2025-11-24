97801edf256a7059836d7f7b60435eb7c017bb8c
Назар Лакуста <n.lakusta@step.local>
2025-11-24 17:31:29 +0200

    exception1221dcx

--- exception/exception les 2.py
+++ exception/exception les 2.py
@@ -1,2 +1,11 @@
 try:
     num = int(input("Введи число: "))
+
+except ValueError:
+    print("Це не число")
+
+else:
+    print("Молодець! Ти нарештів ввів число ",num)
+
+finally:
+    print("Оерація завершена(успіх чи помилка - неважливо)")
